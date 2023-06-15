1cf1cb83b444ecab3db732003840d9ac8b8ceae9
Replit user <>
2023-06-15 11:15:56 +0000

    whole load of stuff

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const keepAlive = require("./server")
+const { Client, IntentsBitField } = require("discord.js")
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ]
+})
+
+keepAlive()
+client.login(process.env['TOKEN'])
+
+
